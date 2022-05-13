ff7a6deb6866a53c98de088c44df11d2a5c6dc9b
Ghostly Ghoul <tomthehuman3@gmail.com>
2022-05-13 14:53:50 +0200

    treess

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,13 +5,16 @@ using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class CharacterMovement : MonoBehaviour {
-  private NavMeshAgent agent;
-  [SerializeField]  private Transform targetObject;
 
-  void Start() {
-    agent = GetComponent<NavMeshAgent>();
+    private NavMeshAgent agent;
+    [SerializeField] private Transform targetObject;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
         
-  }
+    }
+
     /*
   void Update() {
     if (Input.GetMouseButtonDown(0)) {
@@ -28,8 +31,21 @@ public class CharacterMovement : MonoBehaviour {
 
     private void Update()
     {
-        NavMeshHit targetLocation;
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo, Mathf.Infinity))
+            {
+                targetObject.position = hitInfo.point;
+                agent.SetDestination(targetObject.position);
+            }
+        }
+
+
+        /*NavMeshHit targetLocation;
         NavMesh.FindClosestEdge(targetObject.position, out targetLocation, NavMesh.AllAreas);
         agent.SetDestination(targetLocation.position);
+        */
     }
 }
